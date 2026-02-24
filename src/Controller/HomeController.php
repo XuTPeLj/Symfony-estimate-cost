91431ecc8fd06b99478a8f99ad5ed050fe24cdd1
Yuriy Proskurin <yuriy.proskurin@sholding.ru>
2026-02-25 01:08:38 +0700

    2026-02-24 clear: чистка

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         if ($this->getUser()) {
